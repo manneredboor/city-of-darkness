51e06a31084d25a445b1e9398d8263a991286401
Dmitry Podlesny <rezed93@gmail.com>
2018-09-10 18:17:26 +0300

    webpack css loader

--- src/entries/editor.tsx
+++ src/entries/editor.tsx
@@ -1,6 +1,7 @@
 import * as ReactDOM from 'react-dom'
 import * as React from 'react'
 import { Editor } from '../Editor/Editor'
+require('../css/editor.css')
 
 const editorRoot = document.createElement('div')
 document.body.appendChild(editorRoot)
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,15 @@
 const path = require('path')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
 
 const mode = process.env.NODE_ENV || 'production'
 
 module.exports = {
   mode,
   entry: {
-    script: './src/entries/script',
+    main: './src/entries/main',
     editor: './src/entries/editor',
   },
   devtool: process.env.NODE_ENV === 'development' ? 'eval-source-map' : false,
@@ -22,8 +24,15 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
   },
 
+  optimization: {
+    minimizer: [new OptimizeCSSAssetsPlugin({})],
+  },
+
   plugins: [
     new CopyWebpackPlugin([{ from: './static', to: './' }]),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
     new BrowserSyncPlugin({
       host: 'localhost',
       port: 3000,
@@ -41,6 +50,18 @@ module.exports = {
           silent: true,
           useCache: true,
         },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: false,
+            },
+          },
+        ],
       },
     ],
   },
