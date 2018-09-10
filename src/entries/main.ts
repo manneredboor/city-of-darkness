8a559a709b4c13bbe151a9e570a796375a075de8
Dmitry Podlesny <rezed93@gmail.com>
2018-09-11 00:01:28 +0300

    Map

--- src/entries/main.ts
+++ src/entries/main.ts
@@ -1,7 +1,9 @@
 require('vendor/parlin-noise')
 import { Intro } from '../Intro'
+import { Map } from '../Map'
 import { NavBg } from '../Nav'
 import '../Letters'
 
+new Map()
 new NavBg()
 ;(window as any).kwcIntro = new Intro()
