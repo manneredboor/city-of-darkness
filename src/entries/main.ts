5d39c59654290a6e7f72699205c381b7dd532e14
Dmitry Podlesny <rezed93@gmail.com>
2018-09-11 00:00:57 +0300

    Intro mini refactor

--- src/entries/main.ts
+++ src/entries/main.ts
@@ -1,10 +1,7 @@
 require('vendor/parlin-noise')
-import { Intro } from 'Intro'
-import { NavBg } from 'Nav'
-import 'Letters'
+import { Intro } from '../Intro'
+import { NavBg } from '../Nav'
+import '../Letters'
 
 new NavBg()
-const intro = document.querySelector('.kwc-intro')
-if (intro) {
-  ;(window as any).kwcIntro = new Intro(intro as HTMLElement)
-}
+;(window as any).kwcIntro = new Intro()
