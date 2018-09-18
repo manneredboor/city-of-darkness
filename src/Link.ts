3fcc50dcc9162415ac1f369c190fc3efc7597fda
Giyos <giyos.a@gmail.com>
2018-09-18 15:19:11 +0500

    Same page anchor minor refactoring -> need to be checked

--- src/Link.ts
+++ src/Link.ts
@@ -3,13 +3,12 @@ const link = document.querySelectorAll('.nav-item');
 for(let i = 0; i < link.length; i++) {
   link[i].addEventListener('click', e => {
     if (!e.currentTarget) return
-    console.log(e.currentTarget)
+    console.log(e.currentTarget.href)
     
-    // if (location.pathname === this.href.pathname) {
-    //   if (location.href === this.pathname) return
+    // if (location.pathname === link[i].pathname) {
+    //   if (location.href === link[i].href) return
 
-    //   const el = document.querySelector(this.href.hash)
-    //   // if (el) scrollTo(el.getBoundingClientRect().top + scrollPos.value)
+    //   const el = document.querySelector(link[i].hash)
     //   if (el) el.scrollIntoView(true)
     // }
   })
