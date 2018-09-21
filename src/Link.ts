f0a570ffba38b66700e7bc3ed5656d165bd61e49
Giyos <giyos.a@gmail.com>
2018-09-21 16:06:19 +0500

    Link on one page fix

--- src/Link.ts
+++ src/Link.ts
@@ -4,13 +4,13 @@ links.forEach(l =>
   l.addEventListener('click', e => {
     if (e.currentTarget && e.currentTarget instanceof HTMLAnchorElement) {
       console.log(e.currentTarget.href)
+
+      if (location.pathname === e.currentTarget.pathname) {
+        if (location.href === e.currentTarget.href) return
+  
+        const el = document.querySelector(e.currentTarget.hash)
+        if (el) el.scrollIntoView(true)
+      }
     }
-
-    // if (location.pathname === link[i].pathname) {
-    //   if (location.href === link[i].href) return
-
-    //   const el = document.querySelector(link[i].hash)
-    //   if (el) el.scrollIntoView(true)
-    // }
   }),
 )
