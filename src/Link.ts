5fbb5f0fec80c711e1482ecf043277ed444df358
Giyos <giyos.a@gmail.com>
2018-09-21 14:21:18 +0500

    Link.ts changes

--- src/Link.ts
+++ src/Link.ts
@@ -1,15 +1,16 @@
-const link = document.querySelectorAll('.nav-item');
+const links = document.querySelectorAll('.nav-item')
 
-for(let i = 0; i < link.length; i++) {
-  link[i].addEventListener('click', e => {
-    if (!e.currentTarget) return
-    console.log(e.currentTarget.href)
-    
+links.forEach(l =>
+  l.addEventListener('click', e => {
+    if (e.currentTarget && e.currentTarget instanceof HTMLAnchorElement) {
+      console.log(e.currentTarget.href)
+    }
+
     // if (location.pathname === link[i].pathname) {
     //   if (location.href === link[i].href) return
 
     //   const el = document.querySelector(link[i].hash)
     //   if (el) el.scrollIntoView(true)
     // }
-  })
-}
+  }),
+)
